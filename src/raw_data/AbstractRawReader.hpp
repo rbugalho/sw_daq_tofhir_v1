7a8305fe5ef8ccbb83e1bf1388227241f125b90c
Ricardo Bugalho <ricardo@lip.pt>
2020-05-25 13:28:55 +0100

    Add missing file

---
+++ src/raw_data/AbstractRawReader.hpp
@@ -0,0 +1,21 @@
+#ifndef __PETSYS__ABSTRACTRAW_READER_HPP__DEFINED__
+#define __PETSYS__ABSTRACTRAW_READER_HPP__DEFINED__
+
+#include <EventSourceSink.hpp>
+#include <Event.hpp>
+
+#include <vector>
+
+namespace PETSYS {
+	class AbstractRawReader {
+	public:
+		virtual ~AbstractRawReader() {};
+		virtual bool isQDC() = 0;
+		virtual double getFrequency() = 0;
+		virtual int getNSteps() = 0;
+		virtual void getStepValue(int n, float &step1, float &step2) = 0;
+		virtual void processStep(int n, bool verbose, EventSink<RawHit> *pipeline) = 0;
+	};
+}
+
+#endif // __PETSYS__ABSTRACTRAW_READER_HPP__DEFINED__
